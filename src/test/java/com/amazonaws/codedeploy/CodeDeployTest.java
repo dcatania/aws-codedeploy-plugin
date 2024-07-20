443e650db4274b34e9e6adce0801cfd8e05d4580
d.catania <d.catania@reply.it>
2024-07-20 12:42:20 +0200

    aws sdk update to support new regions

--- src/test/java/com/amazonaws/codedeploy/CodeDeployTest.java
+++ src/test/java/com/amazonaws/codedeploy/CodeDeployTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
  * A copy of the License is located at
- * 
+ *
  *  http://aws.amazon.com/apache2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing
@@ -14,8 +14,8 @@
  */
 package com.amazonaws.codedeploy;
 
-import com.gargoylesoftware.htmlunit.WebAssert;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.htmlunit.WebAssert;
+import org.htmlunit.html.HtmlPage;
 import org.jvnet.hudson.test.HudsonTestCase;
 
 public class CodeDeployTest extends HudsonTestCase {
